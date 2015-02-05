227367447577a7fcea299b14e74fd93111a37891
Amr Draz <amr.m.draz@gmail.com>
2015-02-05 09:57:58 +0200

    final chat app

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.use(express.static('.'));
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('chat message', function(msg){
+    console.log('msg:' + msg);
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, function(){
